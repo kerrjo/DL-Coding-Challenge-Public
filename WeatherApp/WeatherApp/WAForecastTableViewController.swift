03b37a1b4db6ced6893cbfc607ab5a1a19070772
JOSEPH KERR <kerrjo@me.com>
2016-08-21 12:25:06 -0400

    Use update For

--- WeatherApp/WeatherApp/WAForecastTableViewController.swift
+++ WeatherApp/WeatherApp/WAForecastTableViewController.swift
@@ -147,51 +147,8 @@ class WAForecastTableViewController: UITableViewController, WAWeatherInfoDelegat
         return cell
     }
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-    
+   
+    // MARK: Helper
     
     
     func imageFor(iconName:String) -> UIImage? {
@@ -215,12 +172,10 @@ class WAForecastTableViewController: UITableViewController, WAWeatherInfoDelegat
                 
                 self.imageCache.setObject(iconImage, forKey: iconName)
                 
-                self.tableView.reloadData()
+                self.updateFor(iconName)
             }
         }
     }
-    
-    
     
     
     func updateFor(iconName:String) -> Void {
