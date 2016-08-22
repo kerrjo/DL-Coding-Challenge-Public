33c7998878e15bdef0d63f2dafa4e3e985971499
JOSEPH KERR <kerrjo@me.com>
2016-08-21 22:18:40 -0400

    Modify weatherInfo object tighter code

--- WeatherApp/WeatherApp/WAWeatherInfo.swift
+++ WeatherApp/WeatherApp/WAWeatherInfo.swift
@@ -14,7 +14,6 @@ protocol WAWeatherInfoDelegate : class {
     func WeatherInfo(controller: WAWeatherInfo, didReceiveDayForecast dayPeriods:[[String : AnyObject]])
     func WeatherInfo(controller: WAWeatherInfo, didReceiveSattelite imageURLs:[String : AnyObject])
     func WeatherInfo(controller: WAWeatherInfo, didReceiveSatteliteImage image:UIImage)
-
 }
 
 class WAWeatherInfo {
@@ -31,81 +30,90 @@ class WAWeatherInfo {
     func getCurrentConditions () {
         
         let urlString = "http://api.wunderground.com/api/1dc7e22fb723f500/conditions/q/\(currentState)/\(currentCity).json"
-        
-        if let wiURL = NSURL(string: urlString) {
-            
-            let sessionConfiguration = NSURLSessionConfiguration.defaultSessionConfiguration()
-            let session = NSURLSession(configuration: sessionConfiguration)
-            let task = session.dataTaskWithURL(wiURL) { data, response, error in
-                
-                if let httpResponse = response as? NSHTTPURLResponse {
-                    
-                    print("HTTP Status Code = \(httpResponse.statusCode)")
-                    
-                    if let jsonResponse = data {
-                        
-                        do {
-                            let responseData = try NSJSONSerialization.JSONObjectWithData(jsonResponse, options:[] ) as! [String : AnyObject]
-                            
-                            if let currentConditionsDict = responseData["current_observation"] as? [String : AnyObject] {
-                                self.currentConditions = currentConditionsDict
-                            }
-                            
-                            self.delegate?.WeatherInfoDidReceiveData(self)
-                            
-                        } catch {
-                            return
-                        }
-                    }
-                }
-                
-            }
-            
-            task.resume()
+        guard let wiURL = NSURL(string: urlString)
+            else {
+                print("Error Invalid URL \(urlString)")
+                return
         }
         
+        let session = NSURLSession(configuration: NSURLSessionConfiguration.defaultSessionConfiguration())
+        let task = session.dataTaskWithURL(wiURL) { data, response, error in
+            
+            // HTTP request assumes NSHTTPURLResponse force cast
+            
+            let httpResponse = response as! NSHTTPURLResponse
+            
+            print("HTTP Status Code = \(httpResponse.statusCode)")
+            
+            if httpResponse.statusCode == 200 {
+                
+                if let jsonResponse = data {
+                    
+                    do {
+                        let responseData = try NSJSONSerialization.JSONObjectWithData(jsonResponse, options:[] ) as! [String : AnyObject]
+                        
+                        if let currentConditionsDict = responseData["current_observation"] as? [String : AnyObject] {
+                            self.currentConditions = currentConditionsDict
+                        }
+                        
+                        self.delegate?.WeatherInfoDidReceiveData(self)
+                        
+                    } catch {
+                        print("Error processing JSON \(error)")
+                    }
+                }
+            }
+            
+        } // dataTaskWithURL completion
+        
+        task.resume()
+        
     }
-    
     
     
     func getForecast () {
         
         let urlString = "http://api.wunderground.com/api/1dc7e22fb723f500/forecast/q/\(currentState)/\(currentCity).json"
+        guard let wiURL = NSURL(string: urlString)
+            else {
+                print("Error Invalid URL \(urlString)")
+                return
+        }
         
-        if let wiURL = NSURL(string: urlString) {
+        let session = NSURLSession(configuration: NSURLSessionConfiguration.defaultSessionConfiguration())
+        let task = session.dataTaskWithURL(wiURL) { data, response, error in
             
-            let sessionConfiguration = NSURLSessionConfiguration.defaultSessionConfiguration()
-            let session = NSURLSession(configuration: sessionConfiguration)
-            let task = session.dataTaskWithURL(wiURL) { data, response, error in
+            // HTTP request assumes NSHTTPURLResponse force cast
+            
+            let httpResponse = response as! NSHTTPURLResponse
+            
+            print("HTTP Status Code = \(httpResponse.statusCode)")
+            
+            if httpResponse.statusCode == 200 {
                 
-                if let httpResponse = response as? NSHTTPURLResponse {
+                if let jsonResponse = data {
                     
-                    print("HTTP Status Code = \(httpResponse.statusCode)")
-                    
-                    if let jsonResponse = data {
+                    do {
+                        let responseData = try NSJSONSerialization.JSONObjectWithData(jsonResponse, options:[] ) as! [String : AnyObject]
                         
-                        do {
-                            let responseData = try NSJSONSerialization.JSONObjectWithData(jsonResponse, options:[] ) as! [String : AnyObject]
-                            
-                            if let forecastDict = responseData["forecast"] as? [String : AnyObject],
+                        if let forecastDict = responseData["forecast"] as? [String : AnyObject],
                             txtForecastDict = forecastDict["txt_forecast"] as? [String : AnyObject],
-                                forecastPeriods = txtForecastDict["forecastday"] as? [[String : AnyObject]]
-                            {
-                                self.delegate?.WeatherInfo(self, didReceiveDayForecast:forecastPeriods)
-                            }
-                            
-                            //print (self.currentConditions)
-                            
-                        } catch {
-                            return
+                            forecastPeriods = txtForecastDict["forecastday"] as? [[String : AnyObject]]
+                        {
+                            self.delegate?.WeatherInfo(self, didReceiveDayForecast:forecastPeriods)
                         }
+                        
+                        //print (self.currentConditions)
+                        
+                    } catch {
+                        print("Error processing JSON \(error)")
                     }
                 }
-                
             }
             
-            task.resume()
         }
+        
+        task.resume()
         
     }
     
@@ -113,109 +121,79 @@ class WAWeatherInfo {
     func getSattelite () {
         
         let urlString = "http://api.wunderground.com/api/1dc7e22fb723f500/satellite/q/\(currentState)/\(currentCity).json"
-        
-        if let wiURL = NSURL(string: urlString) {
-            
-            let sessionConfiguration = NSURLSessionConfiguration.defaultSessionConfiguration()
-            let session = NSURLSession(configuration: sessionConfiguration)
-            let task = session.dataTaskWithURL(wiURL) { data, response, error in
-                
-                if let httpResponse = response as? NSHTTPURLResponse {
-                    
-                    print("HTTP Status Code = \(httpResponse.statusCode)")
-                    
-                    if let jsonResponse = data {
-                        
-                        do {
-                            let responseData = try NSJSONSerialization.JSONObjectWithData(jsonResponse, options:[] ) as! [String : AnyObject]
-                           
-                            //print(responseData)
-                            
-                            if let satteliteDict = responseData["satellite"] as? [String : AnyObject] {
-                                
-                                self.delegate?.WeatherInfo(self, didReceiveSattelite: satteliteDict)
-                                
-//                                let imageURLBaseString = satteliteDict["image_url_vis"]
-//                                let imageURLString = "\(imageURLBaseString!)\(self.apiKey)"
-//                                self.getSatteliteImageAtURL(imageURLString)
-
-                            }
-                            
-                            
-                            
-                        } catch {
-                            return
-                        }
-                    }
-                }
-                
-            }
-            
-            task.resume()
+        guard let wiURL = NSURL(string: urlString)
+            else {
+                print("Error Invalid URL \(urlString)")
+                return
         }
         
+        let session = NSURLSession(configuration: NSURLSessionConfiguration.defaultSessionConfiguration())
+        let task = session.dataTaskWithURL(wiURL) { data, response, error in
+            
+            // HTTP request assumes NSHTTPURLResponse force cast
+            
+            let httpResponse = response as! NSHTTPURLResponse
+            
+            print("HTTP Status Code = \(httpResponse.statusCode)")
+            
+            if httpResponse.statusCode == 200 {
+                
+                if let jsonResponse = data {
+                    
+                    do {
+                        let responseData = try NSJSONSerialization.JSONObjectWithData(jsonResponse, options:[] ) as! [String : AnyObject]
+                        
+                        //print(responseData)
+                        
+                        if let satteliteDict = responseData["satellite"] as? [String : AnyObject] {
+                            
+                            // self.delegate?.WeatherInfo(self, didReceiveSattelite: satteliteDict)
+                            
+                            let imageURLBaseString = satteliteDict["image_url_vis"]
+                            let imageURLString = "\(imageURLBaseString!)\(self.apiKey)"
+                            self.getSatteliteImageAtURL(imageURLString)
+                        }
+                        
+                    } catch {
+                        print("Error processing JSON \(error)")
+                    }
+                }
+            } // 200
+            
+        }
+        
+        task.resume()
+        
     }
-    
     
     
     func getSatteliteImageAtURL (urlString: String) {
         
-        //let urlString = "http://api.wunderground.com/api/1dc7e22fb723f500/satellite/q/\(currentState)/\(currentCity).json"
+        guard let wiURL = NSURL(string: urlString)
+            else {
+                print("Error Invalid URL \(urlString)")
+                return
+        }
         
-        if let wiURL = NSURL(string: urlString) {
+        let session = NSURLSession(configuration: NSURLSessionConfiguration.defaultSessionConfiguration())
+        let task = session.dataTaskWithURL(wiURL) { data, response, error in
             
-            let sessionConfiguration = NSURLSessionConfiguration.defaultSessionConfiguration()
-            let session = NSURLSession(configuration: sessionConfiguration)
-            let task = session.dataTaskWithURL(wiURL) { data, response, error in
+            if let httpResponse = response as? NSHTTPURLResponse {
                 
-                if let httpResponse = response as? NSHTTPURLResponse {
-                    
-                    print("HTTP Status Code = \(httpResponse.statusCode)")
-                    
-                    if let imageData = data,
+                print("HTTP Status Code = \(httpResponse.statusCode)")
+                
+                if let imageData = data,
                     let satImage = UIImage(data: imageData) {
-                        
-                        self.delegate?.WeatherInfo(self, didReceiveSatteliteImage: satImage)
-                    }
                     
-                    
+                    self.delegate?.WeatherInfo(self, didReceiveSatteliteImage: satImage)
                 }
-                
             }
             
-            task.resume()
         }
+        
+        task.resume()
         
     }
 
-
-
-    
 }
-
-
-//    "forecast": {
-//    "txt_forecast": {
-//    "date": "2:00 PM PDT",
-//    "forecastday": [{
-//    "period": 0,
-//    "icon": "partlycloudy",
-//    "icon_url": "http://icons-ak.wxug.com/i/c/k/partlycloudy.gif",
-//    "title": "Tuesday",
-//    "fcttext": "Partly cloudy in the morning, then clear. High of 68F. Breezy. Winds from the West at 10 to 25 mph.",
-//    "fcttext_metric": "Partly cloudy in the morning, then clear. High of 20C. Windy. Winds from the West at 20 to 35 km/h.",
-//    "pop": "0"
-//    }, {
-//    "period": 1,
-//    "icon": "partlycloudy",
-//    "icon_url": "http://icons-ak.wxug.com/i/c/k/partlycloudy.gif",
-//    "title": "Tuesday Night",
-//    "fcttext": "Mostly cloudy. Fog overnight. Low of 50F. Winds from the WSW at 5 to 15 mph.",
-//    "fcttext_metric": "Mostly cloudy. Fog overnight. Low of 10C. Breezy. Winds from the WSW at 10 to 20 km/h.",
-//    "pop": "0"
-//    }, {
-//    "period": 2,
-//    "icon": "partlycloudy
-//
-
 
