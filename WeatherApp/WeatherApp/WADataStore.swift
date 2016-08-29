daf763ffe110441527cc888fe5364fa632b95c67
JOSEPH KERR <kerrjo@me.com>
2016-08-29 10:10:36 -0400

    Remove commented code

--- WeatherApp/WeatherApp/WADataStore.swift
+++ WeatherApp/WeatherApp/WADataStore.swift
@@ -402,69 +402,14 @@ class WADataStore: WAWeatherInfoDelegate {
                                      secondaryItems:secondaryItems,
                                      secondaryDict:secondaryConditionsDict
             )
-
-            
-//            self.delegate?.dataStore(self, didReceiveCurrentConditions:conditionItems,
-//                                     conditionsDict:conditions,
-//                                     primaryItems:primaryItems,
-//                                     primaryDict:primaryConditionsDict
-//            )
         }
         
         dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)) {
             self.imageFor(icon, imageURLString: iconURLString)
         }
-
-
-
-        //  displayLocationDict["full"],
         
     }
     
-//    func WeatherInfo(controller: WAWeatherInfo, didReceiveDayForecast dayPeriods:[[String : AnyObject]]) {
-//        
-//        let forecastPeriods = dayPeriods.sort({ (item1, item2) -> Bool in
-//            let v1 = item1["period"] as! Int
-//            let v2 = item2["period"] as! Int
-//            return v1 < v2
-//        })
-//
-//        delegate?.dataStore(self, didReceiveDayForecast:forecastPeriods)
-//
-//        for result in forecastPeriods {
-//            let icon = result["icon"] as! String
-//            let iconURLString = result["icon_url"] as! String
-//            
-//            self.imageFor(icon, imageURLString: iconURLString)
-//            
-//            //print(result)
-//        }
-//        
-//    }
-//    
-//    func WeatherInfo(controller: WAWeatherInfo, didReceiveDayForecast dayPeriods:[[String : AnyObject]],
-//                     forecastDataPeriods:[[String : AnyObject]])
-//    {
-//        let forecastPeriods = dayPeriods.sort({ (item1, item2) -> Bool in
-//            let v1 = item1["period"] as! Int
-//            let v2 = item2["period"] as! Int
-//            return v1 < v2
-//        })
-//        
-//        //delegate?.dataStore(self, didReceiveDayForecast:forecastPeriods)
-//        
-//        for result in forecastPeriods {
-//            let icon = result["icon"] as! String
-//            let iconURLString = result["icon_url"] as! String
-//            
-//            self.imageFor(icon, imageURLString: iconURLString)
-//            
-//            //print(result)
-//        }
-//        
-//        delegate?.dataStore(self, didReceiveDayForecast:forecastPeriods, forecastDataPeriods: forecastDataPeriods)
-//    
-//    }
     
     func weatherInfo(controller: WAWeatherInfo, didReceiveForecast forecast:[String : AnyObject]) {
         
@@ -634,69 +579,4 @@ class WADataStore: WAWeatherInfoDelegate {
     }
     
 }
-
-
-// Hourly Item
-//
-//    ["snow": {
-//    english = "0.0";
-//    metric = 0;
-//    }, "windchill": {
-//    english = "-9999";
-//    metric = "-9999";
-//    }, "icon": clear, "mslp": {
-//    english = "30.11";
-//    metric = 1020;
-//    }, "wx": Mostly Sunny, "condition": Clear, "pop": 0, "heatindex": {
-//    english = "-9999";
-//    metric = "-9999";
-//    }, "uvi": 4, "dewpoint": {
-//    english = 63;
-//    metric = 17;
-//    }, "wspd": {
-//    english = 13;
-//    metric = 21;
-//    }, "FCTTIME": {
-//    UTCDATE = "";
-//    age = "";
-//    ampm = AM;
-//    civil = "11:00 AM";
-//    epoch = 1472050800;
-//    hour = 11;
-//    "hour_padded" = 11;
-//    isdst = 1;
-//    mday = 24;
-//    "mday_padded" = 24;
-//    min = 00;
-//    "min_unpadded" = 0;
-//    mon = 8;
-//    "mon_abbrev" = Aug;
-//    "mon_padded" = 08;
-//    "month_name" = August;
-//    "month_name_abbrev" = Aug;
-//    pretty = "11:00 AM EDT on August 24, 2016";
-//    sec = 0;
-//    tz = "";
-//    "weekday_name" = Wednesday;
-//    "weekday_name_abbrev" = Wed;
-//    "weekday_name_night" = "Wednesday Night";
-//    "weekday_name_night_unlang" = "Wednesday Night";
-//    "weekday_name_unlang" = Wednesday;
-//    yday = 236;
-//    year = 2016;
-//    }, "feelslike": {
-//    english = 78;
-//    metric = 26;
-//    }, "wdir": {
-//    degrees = 188;
-//    dir = S;
-//    }, "fctcode": 1, "qpf": {
-//    english = "0.0";
-//    metric = 0;
-//    }, "temp": {
-//    english = 78;
-//    metric = 26;
-//    }, "sky": 20, "humidity": 62, "icon_url": http://icons.wxug.com/i/c/k/clear.gif]
-//
-
 
